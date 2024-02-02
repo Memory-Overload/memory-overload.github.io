e74a5d4d217a2b4c73bced6d7ef100c6aa203cef
Kierin <kandrews4@huskers.unl.edu>
2024-02-02 17:19:20 -0600

    updated stuff

--- mo-website/src/Components/Reader/Reader.js
+++ mo-website/src/Components/Reader/Reader.js
@@ -1,24 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./reader.css";
 
+/** Alias for window.sessionStorage.setItem(key, value) */
+const setStorage = (key, value) => window.sessionStorage.setItem(key, value);
+
+/** Alias for window.sessionStorage.getItem(key) */
+const getStorage = (key) => window.sessionStorage.getItem(key);
 
 let chapter_paths_list = [];
 let chapter_titles_list = [];
 
-const get_json = async () =>
-  await fetch("fic_html/" + (window.sessionStorage.getItem("storyPath") ?? 'TheGoodBasiliskLuzura') + "/sitemap.json")
-    .then(resp => resp.json())
-    .then(
-      resp => {
-        chapter_paths_list = [];
-        chapter_titles_list = [];
-        Object.entries(resp)[0][1].map(chap => {
-          chapter_paths_list.push(chap.section.path);
-          chapter_titles_list.push(chap.section.title);
-          return 1;
-        });
-      }
-    );
+// const get_json = async () =>
+//   await fetch("fic_html/" + (getStorage("storyPath") ?? 'TheGoodBasiliskLuzura') + "/sitemap.json")
+//     .then(resp => resp.json())
+//     .then(
+//       resp => {
+//         chapter_paths_list = [];
+//         chapter_titles_list = [];
+//         Object.entries(resp)[0][1].map(chap => {
+//           chapter_paths_list.push(chap.section.path);
+//           chapter_titles_list.push(chap.section.title);
+//           return 1;
+//         });
+//       }
+//     );
 
 let currentChapter = 0;
 let currentFontSize = 16; // default font size
@@ -32,10 +37,20 @@ const fontChoices = [
 let currentFontFamily = fontChoices[0]
 
 function Reader({ folder }) {
-  window.sessionStorage.setItem("storyPath", folder)
+  setStorage("storyPath", folder)
 
   const [isNotFirstChapter, setPreviousButtonVis] = useState(false);
   const [isNotLastChapter, setNextButtonVis] = useState(true);
+  const [chapterDropdown, setChapterDropdown] = useState(null);
+
+  function update_styles(iframe) {
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+
+    // Add a style tag to set the font-size property inside the iframe
+    const styleTag = iframeDocument.createElement('style');
+    styleTag.innerHTML = `body { font-size: ${currentFontSize}px; font-family: ${currentFontFamily};}`;
+    iframeDocument.head.appendChild(styleTag);
+  }
 
   function change_chapter(jumping, new_chapter) {
     // update dropdown and currentChapter
@@ -48,15 +63,7 @@ function Reader({ folder }) {
     iframe.src = "fic_html/" + folder + "/" + chapter_paths_list[currentChapter];
 
     // apply font-size and typeface when iframe loads
-    iframe.onload = () => {
-      // Ensure the content is fully loaded before modifying styles
-      const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-
-      // Add a style tag to set the font-size property inside the iframe
-      const styleTag = iframeDocument.createElement('style');
-      styleTag.innerHTML = `body { font-size: ${currentFontSize}px; font-family: ${currentFontFamily};}`;
-      iframeDocument.head.appendChild(styleTag);
-    };
+    iframe.onload = () => { update_styles(iframe); };
 
     // update button visibility
     setNextButtonVis(currentChapter < (chapter_paths_list.length - 1));
@@ -78,24 +85,60 @@ function Reader({ folder }) {
   }
 
   function toggleWideMode() {
-    let wideMode = window.sessionStorage.getItem("wideMode") ?? "true"
-    wideMode = wideMode === "true" ? "false" : "true";
-    window.sessionStorage.setItem("wideMode", wideMode)
+    // fuck sessionStorage for not allowing booleans
+    let wideMode = getStorage("wideMode") ?? "init"
+    if (wideMode === "init") {
+      wideMode = "true"
+    }
+    else {
+      wideMode = wideMode === "true" ? "false" : "true";
+    }
+    setStorage("wideMode", wideMode)
     const reader = document.getElementById("reader");
     reader.style.maxWidth = wideMode === "true" ? "100vw" : "80vw";
   }
 
-  setTimeout(async () => {
-    await get_json();
-    document.getElementById("reader-iframe").src = "fic_html/" + folder + "/" + chapter_paths_list[0];
-    document.getElementById(currentFontFamily).checked = true;
-    const wide_mode_cb = document.getElementById("wide_mode_checkbox")
-    wide_mode_cb.checked = window.sessionStorage.getItem("wideMode") === "true"
-    if (wide_mode_cb.checked) {
-      const reader = document.getElementById("reader");
-      reader.style.maxWidth = "100vw";
-    }
-  }, 1);
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      // get chapters
+      await fetch("fic_html/" + folder + "/sitemap.json")
+        .then(resp => resp.json())
+        .then(resp => {
+          chapter_paths_list = [];
+          chapter_titles_list = [];
+          Object.entries(resp)[0][1].map(chap => {
+            chapter_paths_list.push(chap.section.path);
+            chapter_titles_list.push(chap.section.title);
+            return 1;
+          });
+        })
+
+      // set wide mode
+      const wide_mode_cb = document.getElementById("wide_mode_checkbox")
+      wide_mode_cb.checked = getStorage("wideMode") === "true"
+      if (wide_mode_cb.checked) {
+        const reader = document.getElementById("reader");
+        reader.style.maxWidth = "100vw";
+      }
+
+      // update iframe src
+      const iframe = document.getElementById("reader-iframe")
+      iframe.src = "fic_html/" + folder + "/" + chapter_paths_list[0];
+      iframe.onload = () => { update_styles(iframe) }
+      if (chapter_titles_list.length > 1) {
+        setChapterDropdown(<select onInput={() => (change_chapter(true, -1))} id="chapter-selection">
+          {[...Array(chapter_paths_list.length).keys()].map((num) =>
+            <option key={num} value={num}>
+              {num + 1}. {chapter_titles_list[num]}
+            </option>)}
+        </select>)
+      }
+      const font_choice = document.getElementsByName("fontSelection")
+      font_choice.forEach(choice => { if (choice.id === String(currentFontFamily)) { choice.checked = true } return 1; })
+    }, 1);
+    return () => clearTimeout(timer);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
@@ -119,22 +162,14 @@ function Reader({ folder }) {
 
       <input id="wide_mode_checkbox" name="wide_mode_checkbox" type="checkbox" onClick={toggleWideMode} />
       <label htmlFor="wide_mode_checkbox">Wide Mode</label>
-
-      {chapter_titles_list.length > 1 &&
+      {chapterDropdown ?
         <p>
           Jump to Chapter...<br />
-          <select onInput={() => (change_chapter(true, 0))} id="chapter-selection">
-            {[...Array(chapter_paths_list.length).keys()].map((num) =>
-              <option key={num} value={num}>
-                {num + 1}. {chapter_titles_list[num]}
-              </option>)}
-          </select>
-        </p>}
-
+          {chapterDropdown}
+        </p> : <></>}
       <div id="reader">
         <iframe id="reader-iframe" title="reader-iframe" src="loading.html"></iframe>
       </div>
-
       {chapter_titles_list.length > 1 ? <p>
         {isNotFirstChapter && <button onClick={() => { change_chapter(false, currentChapter - 1) }}>← Previous Chapter</button>}
         &nbsp;
