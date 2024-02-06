64de60c1faf7ade60d83006fd24a331bd320f3bc
Kierin <kandrews4@huskers.unl.edu>
2024-02-06 00:58:19 -0600

    Remove commented out code in Homepage.js

--- mo-website/src/Components/Homepage/Homepage.js
+++ mo-website/src/Components/Homepage/Homepage.js
@@ -4,9 +4,6 @@ import './Homepage.css';
 function Homepage() {
   return (
     <div>
-      {/* <h3>About Me</h3>
-      <p>Hey, I'm MemoryOverload!</p>
-      <h3>Fanfics I've Written</h3> */}
       <FicList />
     </div>
   )
